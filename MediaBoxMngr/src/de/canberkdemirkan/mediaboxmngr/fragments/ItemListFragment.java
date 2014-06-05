8252a8f5ebee1c5aed17999b178b4db94f630f10
canberk.demirkan <canberk.demirkan@gmail.com>
2014-06-05 23:00:49 +0200

    changed adapter

--- MediaBoxMngr/src/de/canberkdemirkan/mediaboxmngr/fragments/ItemListFragment.java
+++ MediaBoxMngr/src/de/canberkdemirkan/mediaboxmngr/fragments/ItemListFragment.java
@@ -4,27 +4,29 @@ import java.util.ArrayList;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
+import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 import de.canberkdemirkan.mediaboxmngr.R;
 import de.canberkdemirkan.mediaboxmngr.activities.ItemPagerActivity;
 import de.canberkdemirkan.mediaboxmngr.data.DAOItem;
 import de.canberkdemirkan.mediaboxmngr.data.ItemStock;
 import de.canberkdemirkan.mediaboxmngr.model.Item;
+import de.canberkdemirkan.mediaboxmngr.util.ItemAdapter;
 
-public class ItemListFragment extends ListFragment {
+public class ItemListFragment extends Fragment {
 
 	private static final String TAG = "ItemListFragment";
 
 	private ArrayList<Item> mItemList;
+	private ListView mListView;
+	private ItemAdapter mItemAdapter;
 
 	private DAOItem mDAOItem;
 
@@ -38,57 +40,86 @@ public class ItemListFragment extends ListFragment {
 		// mItemList = ItemStock.get(getActivity()).getDAOItem().getAllItems();
 		mDAOItem = ItemStock.get(getActivity()).getDAOItem();
 
-		ItemAdapter adapter = new ItemAdapter(mItemList);
-		setListAdapter(adapter);
+		// ItemAdapter adapter = new ItemAdapter(mItemList);
+		// setListAdapter(adapter);
 	}
 
 	@Override
-	public void onListItemClick(ListView l, View v, int position, long id) {
-		Item item = ((ItemAdapter) getListAdapter()).getItem(position);
-		Intent i = new Intent(getActivity(), ItemPagerActivity.class);
-		i.putExtra(ItemFragment.EXTRA_ITEM_ID, item.getUniqueId());
-		startActivity(i);
-	}
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_item_list, null);
 
-	private class ItemAdapter extends ArrayAdapter<Item> {
+		mListView = (ListView) view
+				.findViewById(R.id.listView_fragmentItemList);
 
-		public ItemAdapter(ArrayList<Item> itemList) {
-			super(getActivity(), 0, itemList);
-		}
+		mItemAdapter = new ItemAdapter(this.getActivity(), mItemList);
+		mItemAdapter.setNotifyOnChange(true);
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
+		mListView.setAdapter(mItemAdapter);
+		mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-			// If we weren't given a view, inflate one
-			if (convertView == null) {
-				convertView = getActivity().getLayoutInflater().inflate(
-						R.layout.list_item, null);
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View view,
+					int position, long id) {
+
+				Item item = (Item) mListView.getAdapter().getItem(position);
+				Intent i = new Intent(getActivity(), ItemPagerActivity.class);
+				i.putExtra(ItemFragment.EXTRA_ITEM_ID, item.getUniqueId());
+				startActivity(i);
 			}
+		});
 
-			// Configure the view for this Item
-			Item item = getItem(position);
-
-			TextView tv_itemTitle = (TextView) convertView
-					.findViewById(R.id.tv_listItem_itemTitle);
-			tv_itemTitle.setText(item.getTitle());
-
-			TextView tv_itemDate = (TextView) convertView
-					.findViewById(R.id.tv_listItem_itemCreationDate);
-			tv_itemDate.setText(item.getCreationDate().toString());
-
-			CheckBox cb_itemFavorite = (CheckBox) convertView
-					.findViewById(R.id.cb_listItem_itemFavorite);
-			cb_itemFavorite.setChecked(item.isFavorite());
-
-			return convertView;
-		}
-
+		return view;
 	}
+
+	// @Override
+	// public void onListItemClick(ListView l, View v, int position, long id) {
+	// Item item = ((ItemAdapter) getListAdapter()).getItem(position);
+	// Intent i = new Intent(getActivity(), ItemPagerActivity.class);
+	// i.putExtra(ItemFragment.EXTRA_ITEM_ID, item.getUniqueId());
+	// startActivity(i);
+	// }
+
+	// private class ItemAdapter extends ArrayAdapter<Item> {
+	//
+	// public ItemAdapter(ArrayList<Item> itemList) {
+	// super(getActivity(), 0, itemList);
+	// }
+	//
+	// @Override
+	// public View getView(int position, View convertView, ViewGroup parent) {
+	//
+	// // If we weren't given a view, inflate one
+	// if (convertView == null) {
+	// convertView = getActivity().getLayoutInflater().inflate(
+	// R.layout.list_item, null);
+	// }
+	//
+	// // Configure the view for this Item
+	// Item item = getItem(position);
+	//
+	// TextView tv_itemTitle = (TextView) convertView
+	// .findViewById(R.id.tv_listItem_itemTitle);
+	// tv_itemTitle.setText(item.getTitle());
+	//
+	// TextView tv_itemDate = (TextView) convertView
+	// .findViewById(R.id.tv_listItem_itemCreationDate);
+	// tv_itemDate.setText(item.getCreationDate().toString());
+	//
+	// CheckBox cb_itemFavorite = (CheckBox) convertView
+	// .findViewById(R.id.cb_listItem_itemFavorite);
+	// cb_itemFavorite.setChecked(item.isFavorite());
+	//
+	// return convertView;
+	// }
+	//
+	// }
 
 	@Override
 	public void onResume() {
 		super.onResume();
-		((ItemAdapter) getListAdapter()).notifyDataSetChanged();
+		// ((ItemAdapter) getListAdapter()).notifyDataSetChanged();
+		mItemAdapter.refresh(mItemList);
 	}
 
 	@Override
---
+++ MediaBoxMngr/src/de/canberkdemirkan/mediaboxmngr/util/ItemAdapter.java
@@ -0,0 +1,69 @@
+package de.canberkdemirkan.mediaboxmngr.util;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import de.canberkdemirkan.mediaboxmngr.R;
+import de.canberkdemirkan.mediaboxmngr.model.Item;
+
+public class ItemAdapter extends ArrayAdapter<Item> {
+
+	// TODO ViewHolder Pattern
+	static class ViewHolder {
+		TextView mTextItemTitle;
+		TextView mTextItemCreationDate;
+		CheckBox mCheckBoxItemFavorite;
+	}
+
+	private final Context mContext;
+	private ArrayList<Item> mItemList;
+
+	public ItemAdapter(Context context, ArrayList<Item> itemList) {
+		super(context, android.R.layout.simple_list_item_1, itemList);
+		this.mContext = context;
+		this.mItemList = itemList;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		LayoutInflater inflater = (LayoutInflater) mContext
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		ViewHolder holder;
+
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.list_item, parent, false);
+			holder = new ViewHolder();
+			holder.mTextItemTitle = (TextView) convertView
+					.findViewById(R.id.tv_listItem_itemTitle);
+			holder.mTextItemCreationDate = (TextView) convertView
+					.findViewById(R.id.tv_listItem_itemCreationDate);
+			holder.mCheckBoxItemFavorite = (CheckBox) convertView
+					.findViewById(R.id.cb_listItem_itemFavorite);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+
+		// Configure the view for this Item
+		Item item = getItem(position);
+		holder.mTextItemTitle.setText(item.getTitle());
+		holder.mTextItemCreationDate.setText(item.getCreationDate().toString());
+		holder.mCheckBoxItemFavorite.setChecked(item.isFavorite());
+
+		return convertView;
+	}
+
+	public void refresh(ArrayList<Item> itemList) {
+		this.mItemList = itemList;
+		notifyDataSetChanged();
+	}
+
+}
